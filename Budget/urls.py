176b6bf4445b22a4dd6fa2794929604aba929036
’3afrit111’ <20240178@stud.fci-cu.edu.eg>
2026-05-04 23:43:53 +0300

    others
    reading between front and back

--- Budget/urls.py
+++ Budget/urls.py
@@ -6,4 +6,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home_page, name='home'),
+    path('login/', views.login_page, name='login_url'),
+    path('signup/', views.signup_page, name='signup_url'),
+    path('dashboard/', views.dashboard_page, name='dashboard_url'),
 ]
--- Budget/views.py
+++ Budget/views.py
@@ -22,3 +22,13 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request, 'home.html')
+
+
+def login_page(request):
+    return render(request, 'login.html')
+
+def signup_page(request):
+    return render(request, 'sign.html')
+
+def dashboard_page(request):
+    return render(request, 'dashboard.html')
