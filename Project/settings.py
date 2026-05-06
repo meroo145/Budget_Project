c6e438acd2cb5e4f7d43ed8a66ffc00cdb892b5e
Mariam <mariemsayedr33@gmail.com>
2026-05-06 21:07:40 +0300

    add env example

--- Project/settings.py
+++ Project/settings.py
@@ -51,6 +51,7 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql', 
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
